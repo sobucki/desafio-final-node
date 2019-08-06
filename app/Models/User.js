3b2907c0a4520477e0c63c39be6251aa3b59abe6
sobucki <sobucki@MacBook-Pro.local>
2019-08-06 11:00:49 -0400

    Rota e migration de usuario

--- app/Models/User.js
+++ app/Models/User.js
@@ -10,11 +10,7 @@ class User extends Model {
   static boot () {
     super.boot()
 
-    /**
-     * A hook to hash the user password before saving
-     * it to the database.
-     */
-    this.addHook('beforeSave', async (userInstance) => {
+    this.addHook('beforeSave', async userInstance => {
       if (userInstance.dirty.password) {
         userInstance.password = await Hash.make(userInstance.password)
       }
