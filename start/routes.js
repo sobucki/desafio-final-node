ad578af890924038738530aa4739736b40e5e84a
Rafael Sobucki <sobucki@gmail.com>
2019-08-06 16:03:02 -0400

    Rota de sessao e recuperacao de senha

--- start/routes.js
+++ start/routes.js
@@ -1,6 +1,9 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
-//teste
-Route.post('users', 'UserController.store')
+const Route = use("Route");
+
+Route.post("users", "UserController.store");
+Route.post("sessions", "SessionController.store");
+
+Route.post("passwords", "ForgotPasswordController.store");
